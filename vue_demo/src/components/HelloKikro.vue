15952ff046dcf4ba6f8b33e7c13472037cefe1d9
wenjiadong <wenjiadong@gaosiedu.com>
2018-06-17 12:59:40 +0800

    day05

---
+++ vue_demo/src/components/HelloKikro.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p class="msg">{{msg}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {
+            msg: 'Hello vue I am kikro, yoyo '
+          }
+        }
+    }
+</script>
+
+<style>
+  .msg{
+    color: red;font-size: 50px;
+  }
+</style>
