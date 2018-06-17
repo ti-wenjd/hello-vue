7a9ea4173deb66d4d96a71e9948e5d7ff7573d52
wenjiadong <wenjiadong@gaosiedu.com>
2018-06-18 03:02:54 +0800

    day 07

---
+++ vue_demo/src-base/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <HelloKikro/>
+  </div>
+</template>
+
+<script>
+
+  import HelloKikro from './components/HelloKikro'
+
+  export default {
+    components: {
+      HelloKikro
+    }
+  }
+</script>
+
+<style>
+
+  .logo {
+    width: 400px;
+    height: 400px;
+  }
+
+</style>
--- vue_demo/src/App.vue
+++ vue_demo/src/App.vue
@@ -1,26 +1,59 @@
 <template>
   <div>
-    <img class="logo" src="./assets/logo.png" alt="logo">
-    <HelloKikro/>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments"/>
+    </div>
   </div>
 </template>
 
 <script>
 
-  import HelloKikro from './components/HelloKikro'
+  import Add from './components/Add'
+  import List from './components/List'
 
   export default {
+    data() {
+      return {
+        comments: [
+          {
+            name: 'Bod',
+            content: 'Vye good'
+          },
+          {
+            name: 'Ccc',
+            content: 'Vye good222'
+          },
+          {
+            name: 'TRX',
+            content: 'Vye good333'
+          }
+        ]
+      }
+    },
+    methods:{
+      //添加评论
+      addComment(comment){
+        this.comments.unshift(comment)
+      }
+
+    },
+    name: "app",
     components: {
-      HelloKikro
+      Add, List
     }
   }
 </script>
 
-<style>
-
-  .logo {
-    width: 400px;
-    height: 400px;
-  }
+<style scoped>
 
 </style>
